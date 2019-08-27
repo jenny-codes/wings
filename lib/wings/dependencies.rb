aadee62e41fab6c340df68bdc5aee1b5228c3267
jennyshih <jinghua.shih@gmail.com>
2019-08-27 11:15:08 +0800

    added a flag to avoid infinite loop between `const_missing` and `const_get`

--- lib/wings/dependencies.rb
+++ lib/wings/dependencies.rb
@@ -1,6 +1,12 @@
 class Object
   def self.const_missing(c)
+    return nil if @calling_const_missing
+
+    @calling_const_missing = true
     require Wings.to_underscore(c.to_s)
-    Object.const_get(c)
+    klass = Object.const_get(c)
+    @calling_const_missing = false
+
+    klass
   end
 end
