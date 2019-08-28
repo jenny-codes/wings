05b5e6403e0decffeebbf4540a2cc770e601823d
jennyshih <jinghua.shih@gmail.com>
2019-08-28 11:44:36 +0800

    added controller-to-view automatic rendering functionality

--- lib/wings.rb
+++ lib/wings.rb
@@ -11,8 +11,11 @@ module Wings
       get_controller_and_action(env) do |controller, action|
         instance = controller.new(env)
         instance.send(action)
-        r = instance.response
-        [r.status, r.headers, r.body]
+        if r = instance.response
+          [r.status, r.headers, r.body]
+        else
+          instance.render(action)
+        end
       end
     end
   end
